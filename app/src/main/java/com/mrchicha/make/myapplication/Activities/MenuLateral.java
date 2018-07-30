20c26a27bc3ad9a08a176deddec1e43dd878662b
mrchicha <mrchciha99@gmail.com>
2018-07-30 20:41:39 +0200

    Añadida opción de compartir dorsal con toggle

--- app/src/main/java/com/mrchicha/make/myapplication/Activities/MenuLateral.java
+++ app/src/main/java/com/mrchicha/make/myapplication/Activities/MenuLateral.java
@@ -90,7 +90,7 @@ public class MenuLateral extends AppCompatActivity implements NavigationView.OnN
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //Toggle item
+        //Opción Toggle item
         Menu menu = navigationView.getMenu();
         MenuItem menuItem = menu.findItem(R.id.app_bar_switch);
         MenuItem locDorsal = menu.findItem(R.id.nav_loc_dorsal);
